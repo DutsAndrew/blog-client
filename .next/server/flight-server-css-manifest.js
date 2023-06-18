9f9ce2f6ee4c0b321b46a3761013e9722cf9ee49
DutsAndrew <dutsandrew@gmail.com>
2023-06-18 16:48:55 -0600

    correct link in readme

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,1 +1,16 @@
-self.__RSC_CSS_MANIFEST={"cssImports":{"/home/andrew/top/blog-client/app/page.tsx":["/home/andrew/top/blog-client/app/page.module.css"],"/home/andrew/top/blog-client/app/layout.tsx":["/home/andrew/top/blog-client/app/globals.css"]},"cssModules":{"/home/andrew/top/blog-client/app/page":["/home/andrew/top/blog-client/app/globals.css","/home/andrew/top/blog-client/app/page.module.css"]}}
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/andrew/top/blog-client/app/page.tsx": [
+      "/home/andrew/top/blog-client/app/page.module.css"
+    ],
+    "/home/andrew/top/blog-client/app/layout.tsx": [
+      "/home/andrew/top/blog-client/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/andrew/top/blog-client/app/page": [
+      "/home/andrew/top/blog-client/app/globals.css",
+      "/home/andrew/top/blog-client/app/page.module.css"
+    ]
+  }
+}
